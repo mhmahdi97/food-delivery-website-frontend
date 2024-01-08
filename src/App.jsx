f6271798913f21a0b8f3e7cfb4e3c2563779af80
mhmahdi97 <mhmahdi97@gmail.com>
2024-01-08 14:36:30 +0600

    created hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Home from "./Components/Home/Home";
 import Navbar from "./Components/Nabvar/Navbar";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
       <div className="bg-primary">
         <div className="max-w-6xl mx-auto p-4 sm:p-9">
           <Navbar />
+          <Home />
         </div>
       </div>
     </>
---
+++ src/Components/Home/Hero.jsx
@@ -0,0 +1,24 @@
+import banner from "../../assets/images/Image1.png";
+
+const Hero = () => {
+    return (
+      <div className="flex flex-col lg:flex-row lg:items-center bg-secondary rounded-3xl p-6 lg:p-20 max-w-full lg:h-[314px]">
+        <div className="hidden md:block lg:text-left text-center">
+          <h1 className="text-heading-primary mb-6">
+            Deliver Food to Your Doorstep!
+          </h1>
+          <p className="text-heading-secondary">
+            Authentic Food, Quick Service, Fast Delivery
+          </p>
+        </div>
+        <div className="mx-auto">
+          <img
+            src={banner}
+            alt="banner"
+          />
+        </div>
+      </div>
+    );
+};
+
+export default Hero;
